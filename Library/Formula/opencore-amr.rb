a0842407d8ebeb27577759ed6359f9ccb21d3afc
Justin Camerer <iamjwc@gmail.com>
2011-01-28 00:48:35 -0500

    Updated broken url in Formula/opencore-amr

--- Library/Formula/opencore-amr.rb
+++ Library/Formula/opencore-amr.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class OpencoreAmr <Formula
-  url 'http://downloads.sourceforge.net/opencore-amr/opencore-amr-0.1.2.tar.gz'
+  url 'http://downloads.sourceforge.net/project/opencore-amr/opencore-amr/0.1.2/opencore-amr-0.1.2.tar.gz'
   homepage 'http://opencore-amr.sourceforge.net/'
   md5 '8e8b8b253eb046340ff7b6bf7a6ccd3e'
 
